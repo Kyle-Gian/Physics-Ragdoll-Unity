082282138372bdac17c004d54330a3d1b8b75412
Kyle Gian <kyle.gian@hotmail.com>
2021-04-01 13:29:44 +1100

    Puzzle colors changing with box collider

---
+++ Physics Unity Project/Assets/Scripts/Mechanics/Puzzle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+
+public class Puzzle
+{
+    public Image m_image;
+    public int m_colorIndex = 0;
+    public Button m_button;
+
+    public void SetcolorIndex(int a_colorIndex)
+    {
+        m_colorIndex = a_colorIndex;
+
+    }
+    public int GetcolorIndex()
+    {
+        return m_colorIndex;
+    }
+}
--- Physics Unity Project/Assets/Scripts/Mechanics/PuzzleScript.cs
+++ Physics Unity Project/Assets/Scripts/Mechanics/PuzzleScript.cs
@@ -3,69 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
-public struct Puzzle
-{
-   public Image m_image;
-   public int m_colorIndex;
-   public Button m_button;
-}
+
 
 public class PuzzleScript : MonoBehaviour
 {
-    int m_colorIndex = 0;
-    
+    [SerializeField] LayerMask buttonLayer;
+
     [SerializeField]
-    List<Puzzle> m_puzzles = null;
-    List<Color> m_listOfColors = null;
+    List<Puzzle> m_puzzles = new List<Puzzle>();
+    List<Color32> m_listOfColors = new List<Color32>();
+
+    Color m_color;
     // Start is called before the first frame update
     void Start()
     {
-        m_listOfColors.Add(new Color(114, 0, 255));
-        m_listOfColors.Add(new Color(255, 36, 0));
-        m_listOfColors.Add(new Color(59, 250, 4));
-        m_listOfColors.Add(new Color(0, 248, 255));
-        m_listOfColors.Add(new Color(255, 246, 0));
+        m_listOfColors.Add(new Color32(114, 0, 255,255));
+        m_listOfColors.Add(new Color32(255, 36, 0,255));
+        m_listOfColors.Add(new Color32(59, 250, 4,255));
+        m_listOfColors.Add(new Color32(0, 248, 255,255));
+        m_listOfColors.Add(new Color32(255, 246, 0,255));
+
+        for (int i = 0; i < m_puzzles.Count; i++)
+        {
+            m_color = new Color32(m_listOfColors[i].r, m_listOfColors[i].g, m_listOfColors[i].b, m_listOfColors[i].a);
+
+            m_puzzles[i].m_image.color = m_color;
+            m_puzzles[i].m_colorIndex = i;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 rayPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
-        //RaycastHit hitInfo;
-
-        //if (Physics.Raycast(rayPos, Camera.main.transform.forward, out hitInfo, 30))
-        //{
-        //    for (int i = 0; i < m_images.Count; i++)
-        //    {
-
-        //        if (hitInfo.collider.gameObject == m_images[i].gameObject.GetComponent<Button>() && Input.GetMouseButtonDown(0))
-        //        {
-        //            m_images[i].color = m_listOfColors[m_colorIndex];
-        //        }
-        //    }
-        //}
-
-        for (int i = 0; i < m_puzzles.Count; i++)
+        if (Input.GetMouseButtonDown(0))
         {
-            m_puzzles[i].m_image.color = m_listOfColors[m_colorIndex];
+            Vector3 rayPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayPos, Camera.main.transform.forward, out hitInfo, 30, buttonLayer))
+            {
+                //hitInfo.transform.GetComponent<Button>().onClick.Invoke();
+
+                for (int i = 0; i < m_puzzles.Count; i++)
+                {
+                    Debug.Log(hitInfo.collider);
+                    if (hitInfo.collider == m_puzzles[i].m_button.GetComponent<BoxCollider>())
+                    {
+                        if (m_puzzles[i].m_colorIndex == m_listOfColors.Count - 1)
+                        {
+                            m_puzzles[i].m_colorIndex = 0;
+                        }
+                        else
+                        {
+                            m_puzzles[i].m_colorIndex++;
+                        }
+                        m_puzzles[i].m_image.color = m_listOfColors[m_puzzles[i].m_colorIndex];
+                    }
+                }
+            }
         }
 
-    }
-    public void ChangeColor()
-    {
-        for (int i = 0; i < m_puzzles.Count; i++)
-        {
-            m_puzzles[1].m_button.onClick ;
-
-        }
-        if (m_colorIndex < m_listOfColors.Count)
-        {
-            m_colorIndex++;
-        }
-        else
-        {
-            m_colorIndex = 0;
-        }
     }
 }
